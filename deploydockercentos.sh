70c762a65cc168d188b7c76d952252ab76972dd1
dmitri166 <dmitre.k166@gmail.com>
2020-02-09 23:45:00 +0200

    Delete deploydockercentos.sh

--- deploydockercentos.sh
+++
@@ -1,6 +0,0 @@
-# update the package database:
-sudo yum check-update
-#add the official Docker repository, download the latest version of Docker, and install it:
-curl -fsSL https://get.docker.com/ | sh
-#start the Docker daemon:
-sudo systemctl start docker
